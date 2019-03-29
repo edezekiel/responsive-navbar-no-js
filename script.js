5f8dd4fbce245aaf92d8caafed4afe514292e947
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-03-29 12:58:29 -0400

    wire up js to html

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('hi there')
+
+let nav = document.querySelector('nav')
+
+let navOptions = document.querySelectorAll('nav a')
+
+function clicked(){
+  console.log('clicked')
+}
+
+navOptions.forEach( a => a.addEventListener('click', () => clicked()))
